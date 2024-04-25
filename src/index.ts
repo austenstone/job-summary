14d7dc5f0dec25d8a8973704ac756c10e8811a06
Austen Stone <austenstone@github.com>
2024-04-24 23:20:37 -0400

    Add GitHub Actions workflow for generating test results summary and job summary file path

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,42 @@
-import { getInput, info } from "@actions/core";
-import { getOctokit } from "@actions/github";
+// import { getInput, info } from "@actions/core";
+// import { getOctokit } from "@actions/github";
+import { access, constants } from "fs/promises";
+// interface Input {
+//   token: string;
+// }
 
-interface Input {
-  token: string;
-}
-
-const getInputs = (): Input => {
-  const result = {} as Input;
-  result.token = getInput("github-token");
-  if (!result.token || result.token === "") {
-    throw new Error("github-token is required");
-  }
-  return result;
-}
+// const getInputs = (): Input => {
+//   const result = {} as Input;
+//   result.token = getInput("github-token");
+//   if (!result.token || result.token === "") {
+//     throw new Error("github-token is required");
+//   }
+//   return result;
+// }
 
 const run = async (): Promise<void> => {
-  const input = getInputs();
-  const octokit = getOctokit(input.token);
-
-  const {
-    data: { login },
-  } = await octokit.rest.users.getAuthenticated();
-
-  info(`Hello, ${login}!`);
+  // const input = getInputs();
+  console.log(`Job summary file path: ${await jobSummaryFilePath()}`)
 };
 
 run();
+
+const SUMMARY_ENV_VAR = 'GITHUB_STEP_SUMMARY'
+export const jobSummaryFilePath = async (): Promise<string> => {
+  const pathFromEnv = process.env[SUMMARY_ENV_VAR]
+  if (!pathFromEnv) {
+    throw new Error(
+      `Unable to find environment variable for $${SUMMARY_ENV_VAR}. Check if your runtime environment supports job summaries.`
+    )
+  }
+
+  try {
+    await access(pathFromEnv, constants.R_OK | constants.W_OK)
+  } catch {
+    throw new Error(
+      `Unable to access summary file: '${pathFromEnv}'. Check if the file has correct read/write permissions.`
+    )
+  }
+
+  return pathFromEnv
+}
